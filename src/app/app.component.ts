8a7b36795cd9bf2dbc3e8c818c480e5053506511
hemankpatwal <hemankpatwalcse@gmail.com>
2025-02-04 23:02:17 +0530

    feat: Add Chart feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { Chart, ChartConfiguration, ChartData, ChartEvent, ChartType, registerables } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NgIf, BaseChartDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'health-challenge-tracker';
+  chartType: ChartType = 'bar';
+  chartData: ChartData<'bar'> = {
+    labels: ['Workout 1', 'Workout 2', 'Workout 3'],
+    datasets: [
+      { data: [30, 45, 60], label: 'Minutes' }
+    ]
+  };
 }
